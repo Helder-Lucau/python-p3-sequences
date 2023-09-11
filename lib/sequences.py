0cc5bfd1b4086ec07e49ac39f378d7ceac4a19f6
Helder-Lucau <helderlucau@gmail.com>
2023-09-11 08:45:39 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+   fib = []
+
+   for i in range(length):
+      if i == 0 or i == 1:
+         fib.append(i)
+      else:
+         n = fib[len(fib)-1] + fib[len(fib)-2]
+         fib.append(n)
+   print(fib)
+
+print_fibonacci(10)
+
